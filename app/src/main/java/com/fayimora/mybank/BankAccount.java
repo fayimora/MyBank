b24319cdfd3ee78957da53dc9bbb363cc346f3e7
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-05-31 07:30:11 +0100

    Refactor balance

--- app/src/main/java/com/fayimora/mybank/BankAccount.java
+++ app/src/main/java/com/fayimora/mybank/BankAccount.java
@@ -1,28 +1,35 @@
 package com.fayimora.mybank;
 
+import java.util.ArrayList;
+
 /**
  * Created by fayimora on 31/05/15.
  */
 public class BankAccount {
     private double mBalance;
+    private ArrayList<Double> mTransactions;
 
     public static final double OVERDRAFT_FEE = 30;
 
     public BankAccount(){
         mBalance = 0.0;
+        mTransactions = new ArrayList<>();
     }
 
     public void withdraw(double amount){
-        mBalance -= amount;
-        if(mBalance < 0)
-            mBalance -= OVERDRAFT_FEE;
+        mTransactions.add(-amount);
+        if(getBalance() < 0)
+            mTransactions.add(-OVERDRAFT_FEE);
     }
 
     public void deposit(double amount){
-        mBalance += amount;
+        mTransactions.add(amount);
     }
 
     public double getBalance(){
-        return mBalance;
+        double sum = 0.0;
+        for(Double t: mTransactions)
+            sum += t;
+        return sum;
     }
 }
